a3fc9b037b3df870426de7a9a1c3fd80f5e84ac2
Willem de Groot <willembike@gmail.com>
2018-04-15 21:42:25 +0200

    Added different result images, fixed Gaussian

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -23,7 +23,7 @@ Intensity medianpixel(const IntensityImage * boxsample)
 	return length % 2 ? allvalues[length / 2] : (allvalues[length / 2] + allvalues[length / 2 - 1]) / 2;
 }
 
-Intensity linear(const IntensityImage * boxsample)
+Intensity average(const IntensityImage * boxsample)
 {
 	int sum = 0;
 	for (int x = 0; x < boxsample->getWidth(); ++x)
@@ -32,17 +32,37 @@ Intensity linear(const IntensityImage * boxsample)
 	return sum / (boxsample->getHeight()*boxsample->getWidth());
 }
 
-Intensity bilinear(const IntensityImage * boxsample)
+Intensity gaussian(const IntensityImage * boxsample)
 {
+	//double d = boxsample->getWidth();
+	//double r = d / 2;
+	//double cumulative = 0.0;
+	//double totalfactor = 0.0;
+	////std::cout << "center: " << r << std::endl;
+	//for (int x = 0; x < d; ++x)
+	//	for (int y = 0; y < d; ++y)
+	//	{
+	//		double x_p = abs(r - x);
+	//		double y_p = abs(r - y);
+	//		double dist = sqrt(x_p*x_p + y_p*y_p);
+	//		double weight = 1 - dist / r;
+	//		cumulative += boxsample->getPixel(x, y) * weight;
+	//		totalfactor += weight;
+	//		//std::cout << "(" << x << ", " << y << "): " << weight << std::endl;
+	//	}
+
 	const double pi = atan(1) * 4;
 	const double e = exp(1);
-	int sigma = boxsample->getWidth();
+	const double sigma = boxsample->getWidth()/3;
+	const double size = boxsample->getWidth();
 	double cumulative = 0;
 	double totalfactor = 0;
-	for (int x = 0; x < sigma; ++x)
-		for (int y = 0; y < sigma; ++y)
+	for (int x = 0; x < size; ++x)
+		for (int y = 0; y < size; ++y)
 		{
-			double factor = (1 / (2 * pi*sigma*sigma))*pow(e, (-((x*x + y*y) / (2 * sigma*sigma))));
+			// calculate the Gaussian value for this pixel (x, y)
+			double factor = (1.0 / (2.0 * pi*sigma*sigma))*pow(e, (-(((x - sigma / 2)*(x - sigma / 2) + (y - sigma / 2)*(y - sigma / 2)) / (2.0 * sigma*sigma))));
+			// add 
 			cumulative += boxsample->getPixel(x, y) * factor;
 			totalfactor += factor;
 		}
@@ -60,7 +80,7 @@ void resize(const IntensityImage & origin, IntensityImage * result, const double
 			for (int j = 0; j < multiplier; ++j)
 				for (int i = 0; i < multiplier; ++i)			
 					boxsample->setPixel(i, j, origin.getPixel(multiplier*x + i, multiplier*y + j));
-			result->setPixel(x, y, bilinear(boxsample));
+			result->setPixel(x, y, gaussian(boxsample));
 		}
 }
 
@@ -72,12 +92,13 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 	int targetsize = 40000;
 	int originsize = image.getWidth() * image.getHeight();
 	
-	if (targetsize < originsize)
-	{
-		double ratio = 1.0 / sqrt(originsize / targetsize);	// ratio tussen oud en nieuw bepalen
+	//if (targetsize < originsize)
+	//{
+		double ratio = 1.0 ;// sqrt(originsize / targetsize);	// ratio tussen oud en nieuw bepalen
 		result->set(ratio * image.getWidth(), ratio * image.getHeight());
 		resize(image, result, ratio);
-	}
+	//}
+	//else { std::cout << image.getWidth() << ", " << image.getHeight() << std::endl; }
 	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
 	std::cout << "Time: " << duration << std::endl;
 	return result;
